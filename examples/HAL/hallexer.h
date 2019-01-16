5bdca501712eb594aeab042d4d230ba3e1ac3fda
Ulrik Sandborg-Petersen <ulrikp@scripturesys.com>
2019-01-16 16:07:48 +0100

    Remove the HAVE_ISTREAM and HAVE_OSTREAM distinction between <[io]stream> and <[io]stream.h>.

--- examples/HAL/hallexer.h
+++ examples/HAL/hallexer.h
@@ -5,7 +5,7 @@
  *
  * Ulrik Petersen
  * Created: 6/23-2007
- * Last update: 6/23-2007
+ * Last update: 1/16-2019
  *
  */
 
@@ -31,11 +31,8 @@
 
 #include <emdros-lconfig.h>
 
-#if HAVE_ISTREAM
 #include <istream>
-#else
-#include <istream.h>
-#endif
+
 
 class HAL_Execution_environment; // Forward declaration
 
