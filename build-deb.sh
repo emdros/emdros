3f6398cdae68b2db601a552376b6354fc49ec488
Ulrik Sandborg-Petersen <ulrikp@scripturesys.com>
2019-01-17 09:19:18 +0100

    Enable building straight from an existing tarball.

--- build-deb.sh
+++ build-deb.sh
@@ -10,10 +10,20 @@
 EMDROS_VERSION=`grep "AC_INIT" configure.ac | awk '{print \$2}' | sed -e 's_\\[__g;s_\\]__g;s_,__g'`
 echo "EMDROS_VERSION = ${EMDROS_VERSION}"
 
-# First make sure we are pristine
-make distclean
-./rmfiles.sh
+# Do we have a tarball?
+if test -f emdros-${EMDROS_VERSION}.tar.gz; then
+    # Yes, we do have the tarball.
+    
+    # Unpack it, and build the package.
+    tar xfzv emdros-${EMDROS_VERSION}.tar.gz && ( cd emdros-${EMDROS_VERSION} && dpkg-buildpackage -rfakeroot -d -us -uc || cd .. )
+else
+    # No, we don't have the tarball.  Make it.
+    
+    # First make sure we are pristine
+    make distclean
+    ./rmfiles.sh
 
-# Then rebuild, making the tarball, unpacking it, and building the .deb
-autoreconf -i && ./configure && make dist && tar xfzv emdros-${EMDROS_VERSION}.tar.gz && ( cd emdros-${EMDROS_VERSION} && dpkg-buildpackage -rfakeroot -d -us -uc || cd .. )
+    # Then rebuild, make the tarball, unpack it, and build the .deb
+    autoreconf -i && ./configure && make dist && tar xfzv emdros-${EMDROS_VERSION}.tar.gz && ( cd emdros-${EMDROS_VERSION} && dpkg-buildpackage -rfakeroot -d -us -uc || cd .. )
+fi
 
