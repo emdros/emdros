8196202205bd3c73c364fa604b80bdaef084493c
Ulrik Sandborg-Petersen <ulrikp@scripturesys.com>
2019-02-06 22:23:59 +0100

    #define SUPPORT_UTF for PCRE.

--- pcre/config.h
+++ pcre/config.h
@@ -329,7 +329,7 @@ sure both macros are undefined; an emulation function will then be used. */
    This will work even in an EBCDIC environment, but it is incompatible with
    the EBCDIC macro. That is, PCRE can support *either* EBCDIC code *or*
    ASCII/UTF-8/16/32, but not both at once. */
-/* #undef SUPPORT_UTF */
+#define SUPPORT_UTF 1
 
 /* Define to any value for valgrind support to find invalid memory reads. */
 /* #undef SUPPORT_VALGRIND */
